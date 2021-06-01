366e0579eff5c966caa0e3cc40fea3ab0c4a66ea
Deepali <deepali@leewayhertz.com>
2021-06-01 15:45:13 +0530

    Archive Screen

---
+++ src/modules/Header/header.js
@@ -0,0 +1,57 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import { Row, Column } from "simple-flexbox";
+const Container = styled.div`
+  width: 100%;
+  height: 74px;
+  background-color: #191d43;
+`;
+const Image = styled.img`
+  padding: 10px;
+  margin-left: 25px;
+`;
+const Span = styled.span`
+  color: #ffffff;
+  font-size: 21px;
+  margin-top: 25px;
+  font-weight: 400;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.19;
+`;
+const Input = styled.input`
+  background-color: #3d4270;
+  border-radius: 4px;
+  height: 41px;
+
+  width: 40%;
+  margin: 16px;
+  color: #ffffff;
+  border: none;
+`;
+const Button = styled.button`
+  color: #ffffff;
+  height: 41px;
+  padding: 10px;
+  margin: 16px;
+
+  border-radius: 4px;
+  background-color: #5760bc;
+  font-size: 18px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 0.28;
+`;
+export default function HeaderComponent() {
+  return (
+    <Container>
+      <Row>
+        <Image src="/images/tweetarchive.svg" />
+        <Span>TweetArchive</Span>
+        <Input type="text" />
+        <Button>Copy</Button>
+      </Row>
+    </Container>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -1,35 +1,38 @@
-import React from 'react';
-import {Router, Route} from 'react-router-dom';
-import {Redirect, Switch} from "react-router";
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import {connect} from "react-redux";
-import {Login, SignUp} from "./modules";
-import {history} from "./managers/history";
+import React from "react";
+import { Router, Route } from "react-router-dom";
+import { Redirect, Switch } from "react-router";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import { connect } from "react-redux";
+import { Login, SignUp } from "./modules";
+import { history } from "./managers/history";
 import BaseComponent from "./modules/baseComponent";
+import Header from "./modules/Header";
+import CardComponent from "./modules/MainComponent";
+import Link from "./modules/TweetlinkComponent";
 
 class Routes extends BaseComponent {
+  componentDidMount() {}
 
-    componentDidMount() {
-
-    }
-
-    render() {
-        return (
-
-            <MuiThemeProvider muiTheme={getMuiTheme()}>
-                <Router history={history}>
-                    <Switch>
-                        <Route exact path={'/'} component={Login}/>
-                        <Route exact path={'/sign-up'} component={SignUp}/>
-                        <Redirect exact from='*' to="/"/>
-                    </Switch>
-                </Router>
-            </MuiThemeProvider>);
-    }
+  render() {
+    return (
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <Router history={history}>
+          <Switch>
+            <Route exact path={"/"} component={Login} />
+            <Route exact path={"/sign-up"} component={SignUp} />
+            <Route exact path={"/header"} component={Header} />
+            <Route exact path={"/maincomponent"} component={CardComponent} />
+            <Route exact path={"/tweetlink"} component={Link} />
+            <Redirect exact from="*" to="/" />
+          </Switch>
+        </Router>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {user: state.user}
+  return { user: state.user };
 };
 export default connect(mapStateToProps)(Routes);
