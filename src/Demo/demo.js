2de410098da88fe10e31a33b08f8062a5c541d88
Deepali <deepali@leewayhertz.com>
2021-06-28 12:17:08 +0530

    commit

--- src/Demo/demo.js
+++ src/Demo/demo.js
@@ -1,124 +1,3 @@
-// import React from "react";
-// import styled from "styled-components";
-// import { Row, Column } from "simple-flexbox";
-// import { makeStyles } from "@material-ui/core/styles";
-// import Paper from "@material-ui/core/Paper";
-// import Grid from "@material-ui/core/Grid";
-// import Container from "@material-ui/core/Container";
-// import Typography from "@material-ui/core/Typography";
-// import Button from "@material-ui/core/Button";
-// import Box from "@material-ui/core/Box";
-// import { Dialog } from "material-ui";
-
-// const Main = styled.div`
-//   display: flex;
-//   justify-content: center;
-// `;
-
-// const useStyles = makeStyles((theme) => ({
-//   main: {},
-//   root: {},
-//   grid: {},
-//   grid2: {},
-//   grid3: {},
-//   span: {
-//     padding: "25px",
-//     textAlign: "center",
-//     display: "flex",
-//     justifyContent: "center",
-//     fontSize: "32px",
-//     fontWeight: "600",
-//     fontStretch: "normal",
-//     fontStyle: "normal",
-//     letterSpacing: "normal",
-//     fontFamily: "Raleway, sans-serif !important",
-//     lineHeight: "1.16",
-//     letterSpacing: "normal",
-//     color: " #09184b",
-//   },
-//   input: {
-//     borderRadius: "4px",
-//     background: "url(/images/search.svg) no-repeat 10px",
-//     border: "solid 1px #aab1ff",
-//     padding: "5px",
-//     width: " 37%",
-//   },
-//   button: {
-//     backgroundColor: "#3366ff",
-//     borderRadius: "4px",
-//     color: "#ffffff",
-//     border: "none",
-//     fontSize: "15px",
-//   },
-// }));
-// const Image = styled.img`
-//   width: 55%;
-// `;
-
-// const FakeNewsImage = styled.img`
-//   width: 50%;
-// `;
-
-// const ImageContainer = styled.div`
-//   margin-top: 50px;
-// `;
-
-// const Label = styled.label`
-//   font-size: 14px;
-//   margin-top: 14px;
-//   margin-left: 12px;
-// `;
-
-// const FakeNewsLabel = styled.label`
-//   margin-top: 13px;
-//   font-size: 14px;
-// `;
-
-// const SubContent = styled.span`
-//   margin-top: 10px;
-//   font-size: 12px;
-//   text-align: center;
-// `;
-// export default function DemoComponent() {
-//   const classes = useStyles();
-//   return (
-//     <div className={classes.main}>
-//       <div className={classes.root}>
-//         <Grid container>
-//           <Grid item xs={12}>
-//             <Row className="justify-content-center">
-//               <span className={classes.span}>Enter Tweet Link</span>
-//             </Row>
-//           </Grid>
-//           <Grid item xs={12}>
-//             <Row className="justify-content-center">
-//               <input className={classes.input} type="text" />
-//               <button className={classes.button}>Archive</button>
-//             </Row>
-//           </Grid>
-//           <Row className="justify-content-center">
-//             <Grid item xs={12} sm={12} md={4}>
-//               <Row>
-//                 <img src="images/Fake.svg" />
-//               </Row>
-//             </Grid>
-//             <Grid item xs={12} sm={12} md={4}>
-//               <Row>
-//                 <img src="images/archivetweet.svg" />
-//               </Row>
-//             </Grid>
-//             <Grid item xs={12} sm={12} md={4}>
-//               <Row>
-//                 <img src="images/Legalnews.svg" />
-//               </Row>
-//             </Grid>
-//           </Row>
-//         </Grid>
-//       </div>
-//     </div>
-//   );
-// }
-
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
@@ -175,12 +54,14 @@ const useStyles = makeStyles((theme) => ({
     paddingTop: "56.25%", // 16:9
     alignItems: "center",
     width: "72%",
+    boxShadow: "none",
   },
   cardContent: {
     flexGrow: 1,
     textAlign: "center",
     outline: "none !important",
     border: "none !important",
+    boxShadow: "none",
   },
 
   input: {
