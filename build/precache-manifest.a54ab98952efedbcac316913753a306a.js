31e8909fff2cd30af4333221a4779f1ceb12448a
Abhey gupta <abheyygupta396@gmail.com>
2021-09-25 18:45:21 +0530

    build

---
+++ build/precache-manifest.a54ab98952efedbcac316913753a306a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4785d31b462113a6d19f4e52163461cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36afb0b8bc8651e4f238",
+    "url": "/static/css/2.be18e6ca.chunk.css"
+  },
+  {
+    "revision": "2e256df892ed92179e6a",
+    "url": "/static/css/main.d7acba70.chunk.css"
+  },
+  {
+    "revision": "36afb0b8bc8651e4f238",
+    "url": "/static/js/2.a2ba06da.chunk.js"
+  },
+  {
+    "revision": "76899207e78c257489447ce6d540a529",
+    "url": "/static/js/2.a2ba06da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e256df892ed92179e6a",
+    "url": "/static/js/main.2a1f151a.chunk.js"
+  },
+  {
+    "revision": "5052a09244e1bd5996e3",
+    "url": "/static/js/runtime-main.86a97f0c.js"
+  }
+]);
